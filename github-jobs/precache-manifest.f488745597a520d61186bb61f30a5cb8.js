55f5ad716b745c96226264b1141fda794b5a4afd
Aniket Kudale <aniket.kudale@hotmail.com>
2020-07-22 22:29:01 +0530

    22072020: Added github-jobs app

---
+++ github-jobs/precache-manifest.f488745597a520d61186bb61f30a5cb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbe57824f705b6fe69a6415ae2a5c04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7de9ece5e068f510233",
+    "url": "/static/js/2.27ca5b1b.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.27ca5b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd843114dfed7d182c0b",
+    "url": "/static/js/main.00fb9b90.chunk.js"
+  },
+  {
+    "revision": "fd5ca2d83b2880fa4599",
+    "url": "/static/js/runtime-main.28dc6147.js"
+  }
+]);
